95c12e223f198d804312fd0bb1d86adb6a185386
comger <comger@163.com>
2021-08-26 06:48:25 +0800

    Define Repo interface

--- domain/repo/init.go
+++ domain/repo/init.go
@@ -5,35 +5,58 @@ import (
 	"ucenter/domain/ov"
 )
 
-type IRepo interface {
-	Save(*interface{})
-	FindByID(*ov.OID) *interface{}
-	Fetch(*interface{}) []interface{}
-}
-
 type IUserRepo interface {
-	Save(*entity.User)
-	FindByID(*ov.OID) *entity.Person
-	FindByUserName(*ov.UserName) *entity.User
+	Create(*entity.User)
+	Update(*entity.User)
+	FindByID(ov.UID) *entity.Person
+	FindByUserName(ov.UserName) *entity.User
 	Fetch(*entity.Person) entity.Persons
 }
 
-type ITenantRepo interface {
-	IRepo
-}
-
-type IApplicationRepo interface {
-	IRepo
-}
-
-type IRoleRepo interface {
-	IRepo
-}
-
-type ITenantUser interface {
-	IRepo
+type ICustomRepo interface {
+	Create(*entity.Custom)
+	Update(*entity.Custom)
+	FindByID(ov.CustomID) *entity.Custom
+	Fetch(string) []*entity.Custom
 }
 
 type IGroup interface {
-	IRepo
+	Create(*entity.Group)
+	Update(*entity.Group)
+	FindByID(ov.GroupID) []*entity.Group
+}
+
+type ITenantRepo interface {
+	Create(*entity.Tenant)
+	Update(*entity.Tenant)
+	FindByID(ov.TenantID) *entity.Tenant
+	FindByClientID(ov.ClientID) []*entity.Tenant
+	FindByCustomID(ov.CustomID) []*entity.Tenant
+}
+
+type IApplicationRepo interface {
+	Create(*entity.Application)
+	Update(*entity.Application)
+	FindByID(ov.ClientID) *entity.Application
+	Fetch(string) []*entity.Application
+}
+
+type IRoleRepo interface {
+	Create(*entity.Role)
+	Update(*entity.Role)
+	FindByID(ov.RoleID) *entity.Role
+	Fetch(string) []*entity.Role
+}
+
+type ITenantUser interface {
+	Create(*entity.TenantUser)
+	Update(*entity.TenantUser)
+	FindByID(ov.TenantID, ov.UID) *entity.TenantUser
+	Fetch() []*entity.TenantUser
+}
+
+type IEventLog interface {
+	Create(*entity.EventLog)
+	FindByID(ov.TenantID, ov.UID) []*entity.EventLog
+	FindByEventID(ov.OID) []*entity.EventLog
 }
